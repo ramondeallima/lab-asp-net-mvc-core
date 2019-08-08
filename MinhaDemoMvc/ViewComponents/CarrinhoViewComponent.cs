355a4e6bd7dbd5d7615ce7264b5ddfbdc0edf9c7
Ramon Almeida <ramondeallima@gmail.com>
2019-08-07 23:59:31 -0300

    Usando View Componentns

---
+++ MinhaDemoMvc/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMvc.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensCarrinhos { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            ItensCarrinhos = 3;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsyc()
+        {
+            return View(ItensCarrinhos);
+        }
+
+    }
+}
